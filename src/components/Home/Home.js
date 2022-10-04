10e83d4c2806732284e58bd4f84fe8a93494a1cb
shahadot99999 <shahadot99999@gmail.com>
2022-10-04 15:43:16 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <h3>This is the real home components</h3>
+        </div>
+    );
+};
+
+export default Home; 
---
+++ src/components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Products = () => {
+    return (
+        <div>
+
+            <h4>This is my best product</h4>
+        </div>
+    );
+};
+
+export default Products;
